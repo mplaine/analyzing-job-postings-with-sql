2f978d48f30e214afc2f9e6a0ec18a3752638ea4
Markku Laine <markku.laine@gmail.com>
2024-03-17 12:37:27 +0200

    refactor(dataset): rename tables and columns for improve usability

--- sql/0_init.sql
+++ sql/0_init.sql
@@ -1,22 +1,22 @@
--- Create company_dim table with primary key
-CREATE TABLE company_dim (
-  company_id INT PRIMARY KEY,
+-- Create companies table with primary key
+CREATE TABLE companies (
+  id INT PRIMARY KEY,
   name TEXT,
   link TEXT,
   link_google TEXT,
   thumbnail TEXT
 );
 
--- Create skills_dim table with primary key
-CREATE TABLE skills_dim (
-  skill_id INT PRIMARY KEY,
-  skills TEXT,
+-- Create skills table with primary key
+CREATE TABLE skills (
+  id INT PRIMARY KEY,
+  name TEXT,
   type TEXT
 );
 
--- Create job_postings_fact table with primary key
-CREATE TABLE job_postings_fact (
-  job_id INT PRIMARY KEY,
+-- Create jobs table with primary key
+CREATE TABLE jobs (
+  id INT PRIMARY KEY,
   company_id INT,
   job_title_short VARCHAR(255),
   job_title TEXT,
@@ -32,25 +32,25 @@ CREATE TABLE job_postings_fact (
   salary_rate TEXT,
   salary_year_avg NUMERIC,
   salary_hour_avg NUMERIC,
-  FOREIGN KEY (company_id) REFERENCES company_dim (company_id)
+  FOREIGN KEY (company_id) REFERENCES companies (id)
 );
 
--- Create skills_job_dim table with a composite primary key and foreign keys
-CREATE TABLE skills_job_dim (
+-- Create job_skills table with a composite primary key and foreign keys
+CREATE TABLE job_skills (
   job_id INT,
   skill_id INT,
   PRIMARY KEY (job_id, skill_id),
-  FOREIGN KEY (job_id) REFERENCES job_postings_fact (job_id),
-  FOREIGN KEY (skill_id) REFERENCES skills_dim (skill_id)
+  FOREIGN KEY (job_id) REFERENCES jobs (id),
+  FOREIGN KEY (skill_id) REFERENCES skills (id)
 );
 
 -- Create indexes on foreign key columns for better performance
-CREATE INDEX idx_company_id ON job_postings_fact (company_id);
-CREATE INDEX idx_skill_id ON skills_job_dim (skill_id);
-CREATE INDEX idx_job_id ON skills_job_dim (job_id);
+CREATE INDEX idx_company_id ON jobs (company_id);
+CREATE INDEX idx_job_id ON job_skills (job_id);
+CREATE INDEX idx_skill_id ON job_skills (skill_id);
 
--- Import data to corresponding tables
-COPY company_dim FROM '/docker-entrypoint-initdb.d/company_dim.csv' DELIMITER ',' CSV HEADER;
-COPY skills_dim FROM '/docker-entrypoint-initdb.d/skills_dim.csv' DELIMITER ',' CSV HEADER;
-COPY job_postings_fact FROM '/docker-entrypoint-initdb.d/job_postings_fact.csv' DELIMITER ',' CSV HEADER;
-COPY skills_job_dim FROM '/docker-entrypoint-initdb.d/skills_job_dim.csv' DELIMITER ',' CSV HEADER;
+-- Import data into corresponding tables
+COPY companies FROM '/docker-entrypoint-initdb.d/companies.csv' DELIMITER ',' CSV HEADER;
+COPY skills FROM '/docker-entrypoint-initdb.d/skills.csv' DELIMITER ',' CSV HEADER;
+COPY jobs FROM '/docker-entrypoint-initdb.d/jobs.csv' DELIMITER ',' CSV HEADER;
+COPY job_skills FROM '/docker-entrypoint-initdb.d/job_skills.csv' DELIMITER ',' CSV HEADER;
